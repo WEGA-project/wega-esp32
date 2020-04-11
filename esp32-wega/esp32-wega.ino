f36d8433c29d1632a46a0563b5645b07f950cf0b
Sergey Ivanov <siv237@gmail.com>
2020-04-11 13:43:04 +1000

    Добавлен опрос ЕС

--- esp32-wega/esp32-wega.ino
+++ esp32-wega/esp32-wega.ino
@@ -1,4 +1,3 @@
-
 #include <WiFi.h>
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
@@ -10,9 +9,9 @@ WebServer server(80);
 #include <HTTPClient.h>
 
 const char* ssid = "dd-wrt";
-const char* password = "PASSWORD";
+const char* password = "HruBse1447209";
 
-float pH,pHraw,tempRAW,dtem1;
+float pH,pHraw,tempRAW,dtem1,dst;
 
 // Библиотека работы с DS18b20
 #include <OneWire.h> 
@@ -26,6 +25,7 @@ DallasTemperature sensors(&oneWire);
 #include <AM2320.h>
 AM2320 th;
 float am2320temp, am2320hum;
+double Ap,An;
 
 void setup() {
   Serial.begin(9600);
@@ -96,18 +96,24 @@ void loop() {
   b=(-y2+y1)/(-x2+x1);
   
 //pH shield
-  pHraw=AnalogReadMid(33,500000);
-  pH=a+b*pHraw;
+//  pHraw=AnalogReadMid(33,5);
+//  pH=a+b*pHraw;
   
 //BS18B20
  sensors.requestTemperatures();
  dtem1=sensors.getTempCByIndex(0); 
- tempRAW=AnalogReadMid(32,500000);
+tempRAW=AnalogReadMid(32,50);
 
 // DHT2320
  th.Read();
   am2320temp = th.t;
   am2320hum = th.h;
+
+// EC
+ec(18,19,33,500000);
+
+// Level
+dst=us(13,14,25,5);
  
 WiFiClient client;
 HTTPClient http;
@@ -119,6 +125,9 @@ httpstr +=  "&tempRAW=" + fFTS(tempRAW,3);
 httpstr +=  "&dtem1=" + fFTS(dtem1,3);
 httpstr +=  "&am2320temp=" +fFTS(am2320temp, 1);
 httpstr +=  "&am2320hum=" +fFTS(am2320hum, 1);
+httpstr +=  "&Ap=" +fFTS(Ap, 3);
+httpstr +=  "&An=" +fFTS(An, 3);
+httpstr +=  "&Dst=" +fFTS(dst, 3);
 
 http.begin(client, httpstr);
 http.GET();
@@ -136,6 +145,9 @@ String httpstr="<meta http-equiv='refresh' content='10'>";
        httpstr +=  "Analog temperature RAW: " + fFTS(tempRAW,3)+ "<br>";
        httpstr +=  "am2320temp0: " +fFTS(am2320temp, 1) + "<br>";
        httpstr +=  "am2320hum0: " +fFTS(am2320hum, 1) + "<br>";
+       httpstr +=  "Ap: " +fFTS(Ap, 3) + "<br>";
+       httpstr +=  "An: " +fFTS(An, 3) + "<br>";
+       httpstr +=  "Dst_raw: " +fFTS(dst, 3) + "<br>";
        
 server.send(200, "text/html",  httpstr);
 
@@ -162,4 +174,94 @@ float AnalogReadMid(int port, long count) {
   sensorValue = (analogRead(port))+sensorValue;
  }
  return sensorValue/n;
+
 }
+
+void ec(char d1,  char d2, char a0, long l) {
+
+    Ap=0;
+    An=0;
+  double n = 0;
+
+    pinMode(a0, INPUT_PULLUP);
+    pinMode(d1, OUTPUT);
+    pinMode(d2, OUTPUT);
+
+
+  while (n < l) {
+    n++;
+            
+                  //ArduinoOTA.handle();    
+    digitalWrite(d1, HIGH);
+    delayMicroseconds(1);
+    Ap = analogRead(a0)+Ap; 
+    digitalWrite(d1, LOW); 
+    
+    ArduinoOTA.handle();
+    
+    digitalWrite(d2, HIGH);
+    delayMicroseconds(1);
+    An = analogRead(a0)+An; 
+    digitalWrite(d2, LOW); 
+
+    ArduinoOTA.handle(); 
+   
+
+  }
+    //delay(300);
+    pinMode(d1, INPUT);
+    pinMode(d2, INPUT);
+    pinMode(a0, INPUT);
+
+    Ap=Ap/n;
+    An=An/n;
+    
+    ArduinoOTA.handle();
+
+}
+
+// Функция прямого опроса ультразвукового датчика возврат в микросекундах эха
+float us(int trg, int ech, float temp, long cnt) {
+   long count=0;
+ long microssum=0;
+ while (count < cnt) {
+  count++;
+    ArduinoOTA.handle();
+       
+    pinMode(trg, OUTPUT );
+    pinMode(ech, INPUT);
+
+    digitalWrite(trg,1);
+    delayMicroseconds(10);
+    digitalWrite(trg,0);
+
+
+    long n=0;
+    long limit=100000;
+    
+    long startmic,endmicros;
+    while (n<limit){
+      n++;
+    if(digitalRead(ech) == 1)  
+      {
+      startmic=micros();
+      long z=0;
+      while (digitalRead(ech) == 1 and z<20000) {
+        z++;
+        endmicros=micros();
+        }
+        n=limit;
+        delay(200);
+      } 
+
+    }
+
+  microssum=microssum+(endmicros-startmic);
+      
+ }
+ 
+ float vSound=20.046796*sqrt(273.15+temp);
+ return (vSound/10000)*((float(microssum)/count)/2);
+ //return (float(microssum)/count);
+}
+
